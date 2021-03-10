e8c86a45abd402b51221b24cbfcd11ae9b6c327c
iusmanof <74846667+iusmanof@users.noreply.github.com>
2021-03-10 09:33:57 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+  if (isNaN(n)) {
+    throw new Error("Is not a number");
+  } else {
+    return parseInt(n.toString().split("").reverse().join(""));
+  }
+};
